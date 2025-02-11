b2ac2fd08815fdba4027dee80a97d79822aa594d
Neel Patel <neel@Neels-MBP.lan>
2025-02-11 14:40:37 +0000

    Update config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ["@tanstack/react-query"], // for react-query
   webpack(config, { isServer }) {
     if (!isServer) {
       config.resolve.fallback = { fs: false };
